c8e8a7d7dd2620a10f3719904420fe4a2cf0cac1
Chernetm <136773904+Chernetm@users.noreply.github.com>
2025-04-18 15:28:27 +0300

    prisma new version

--- utils/prisma.js
+++ utils/prisma.js
@@ -1,17 +1,20 @@
+// const { PrismaClient } = require('@prisma/client');
+
+// // const prisma = new PrismaClient();
+
+// module.exports = { prisma };
+
 const { PrismaClient } = require('@prisma/client');
 
-const prisma = new PrismaClient();
+let prisma;
+
+if (process.env.NODE_ENV === 'production') {
+  prisma = new PrismaClient();
+} else {
+  if (!global.prisma) {
+    global.prisma = new PrismaClient();
+  }
+  prisma = global.prisma;
+}
 
 module.exports = { prisma };
-
-// utils/prisma.js
-// import { PrismaClient } from '@prisma/client';
-
-// const prisma = globalThis.prisma || new PrismaClient();
-
-// if (process.env.NODE_ENV !== 'production') {
-//   globalThis.prisma = prisma;
-// }
-
-// export { prisma };
-
