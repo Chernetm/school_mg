ac0b35752a95a36e7a0dd94d37c3998c30cdcedc
Chernetm <136773904+Chernetm@users.noreply.github.com>
2025-04-21 11:21:43 +0300

    staff token

--- app/api/teacher/announcement/route.js
+++ app/api/teacher/announcement/route.js
@@ -4,8 +4,17 @@ const SECRET_KEY = process.env.JWT_SECRET || "your_secret_key";
 
 export async function GET(req) {
   try {
-    const gradeId= req.headers.get("x-user-grade");
-    console.log("Grade from headers:", gradeId);
+    const cookieStore = await Cookies();
+    const token = cookieStore.get("studentToken");
+
+    if (!token) {
+      return Response.json({ error: "Unauthorized" }, { status: 401 });
+    }
+
+    const decoded = jwt.verify(token, SECRET_KEY);
+    const gradeId = decoded.grade;
+    console.log("Decoded Token:", decoded);
+    console.log("Grade ID:", gradeId);
 
     const announcements = await prisma.announcement.findMany({
       where: {
